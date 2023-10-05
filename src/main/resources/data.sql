b9f94a9e020c5ab451df81231d630c797a569203
sabdurakhmanov <sabdurakhmanov@inleksys.ru>
2023-10-05 09:42:26 +0300

    db filling

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,15 @@
 insert into main.authors(id, name)
-values (1, 'А. Дюма'),
-       (2, 'Д.И. Крылов')
+values (1, 'A. Dumas'),
+       (2, 'D.I. Krilov')
 ;
 
 insert into main.genres(id, title)
-values (1, 'Роман'),
-       (2, 'Басня')
+values (1, 'Roman'),
+       (2, 'Fable')
 ;
 
 insert into main.books(id, author, title, genre)
-values (1, 1, 'Три мушкетёра', 1),
-       (2, 1, 'Двадцать лет спустя', 1),
-       (3, 2, 'Мартышка и очки', 2)
+values (1, 1, 'Three Musketeers', 1),
+       (2, 1, 'Twenty years later', 1),
+       (3, 2, 'The Monkey and the Glasses', 2)
 ;
